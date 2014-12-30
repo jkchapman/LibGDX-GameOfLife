e332c21b55470ce245d070dddc04106a81b63208
Jacob Chapman <chapman.j94@gmail.com>
2014-12-30 09:53:44 +0800

    initial libgdx generation

---
+++ html/src/com/jacobkchapman/gameoflife/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.jacobkchapman.gameoflife.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.jacobkchapman.gameoflife.GameOfLife;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new GameOfLife();
+        }
+}
