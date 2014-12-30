5c7f69fabdcfe2962284edcff77a2d09fe1892f5
Jacob Chapman <chapman.j94@gmail.com>
2014-12-30 10:05:15 +0800

    initial window setup

--- core/src/com/jacobkchapman/gameoflife/GameOfLife.java
+++ core/src/com/jacobkchapman/gameoflife/GameOfLife.java
@@ -1,27 +1,22 @@
 package com.jacobkchapman.gameoflife;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
-public class GameOfLife extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class GameOfLife extends Game {
+	public ShapeRenderer shape;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shape = new ShapeRenderer();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(255, 255, 255, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		
 	}
 }
--- desktop/src/com/jacobkchapman/gameoflife/desktop/DesktopLauncher.java
+++ desktop/src/com/jacobkchapman/gameoflife/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.jacobkchapman.gameoflife.GameOfLife;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Conway's Game of Life";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new GameOfLife(), config);
 	}
 }
